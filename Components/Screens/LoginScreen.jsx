fe878a9c37fa78e27e49fd72f8d471752e3e7aa1
Erik Conradie <113360820+EConradie@users.noreply.github.com>
2024-05-15 18:16:01 +0200

    Icons

--- Components/Screens/LoginScreen.jsx
+++ Components/Screens/LoginScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Image} from 'react-native'
 
 import { Colors } from '../Styles'
 
@@ -9,7 +9,7 @@ export const LoginScreen = () => {
       <ScrollView style={MainStyles.scrollContainer}>
         <View style={MainStyles.container}>
             <View style={MainStyles.header}>
-                <Text style={MainStyles.title}>Login</Text>
+                <Image source={require('../../assets/Icons/logo-name.svg')} style={MainStyles.logo} />
             </View>
         </View>
         </ScrollView>
@@ -17,9 +17,6 @@ export const LoginScreen = () => {
 }
 
 const MainStyles = StyleSheet.create({
-  scrollContainer: {
-    flex: 1,
-},
  container: {
     display: 'flex',
     flexDirection: 'column',
@@ -39,14 +36,7 @@ const MainStyles = StyleSheet.create({
     fontSize: 20,
     fontWeight: '500',
   },
-  cardContainer: {
-    display: 'flex',
-    alignItems: 'flex-start',
-    alignContent: 'flex-start',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    gap: 25
-  }
+ 
 })
 
 
--- Components/Screens/SplashScreen.jsx
+++ Components/Screens/SplashScreen.jsx
@@ -1,16 +1,27 @@
 import React from "react";
-import { View, Text, StyleSheet, ScrollView, Pressable, ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Pressable,
+  ImageBackground,
+  Image
+} from "react-native";
 import BackgroundImage from "../../assets/Images/SplashImage.jpg";
+import LogoImage from "../../assets/Icons/logo-name.svg";
+import { Colors } from "../Styles";
 
 export const SplashScreen = () => {
-    return (
-        <ImageBackground
-          source={require(BackgroundImage)}
-          style={SplashStyles.container}
-        >
-          <Text>Hello</Text>
-        </ImageBackground>
-      );
+  return (
+    <ImageBackground source={BackgroundImage} style={SplashStyles.background}>
+      <View style={SplashStyles.overlay}>
+        <View style={SplashStyles.container}>
+            <Image source={LogoImage} style={SplashStyles.logo} />
+        </View>
+      </View>
+    </ImageBackground>
+  );
 };
 
 const SplashStyles = StyleSheet.create({
@@ -18,10 +29,27 @@ const SplashStyles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: 65,
     marginLeft: 30,
     marginRight: 30,
     marginBottom: 30,
-    gap: 25,
   },
+  background: {
+    display: "flex",
+    flexDirection: "column",
+    flex: 1,
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0, 0, 0, 0.5)", // Black with 50% opacity
+  },
+  text:{
+    color: Colors.white
+  },
+  logo:{
+    width: 200,  
+    height: 50, 
+  }
 });
